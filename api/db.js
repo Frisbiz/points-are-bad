ced1ca4e5fa0a2453b508c9875b308a75eb95d49
Faris <fariswy2@gmail.com>
2026-03-04 06:54:31 -0800

    Allow backup: key prefix in db proxy

--- api/db.js
+++ api/db.js
@@ -13,7 +13,7 @@ if (!getApps().length) {
 
 const db = getFirestore();
 
-const ALLOWED_PREFIXES = ["user:", "group:", "groupcode:", "reset:", "useremail:"];
+const ALLOWED_PREFIXES = ["user:", "group:", "groupcode:", "reset:", "useremail:", "backup:"];
 
 function validKey(key) {
   return typeof key === "string" && key.length <= 200 && ALLOWED_PREFIXES.some(p => key.startsWith(p));
